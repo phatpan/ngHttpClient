3d7053f57f9b88a61a772c52088af5076c462e55
phatpan <phat.pan@gmail.com>
2017-09-10 12:29:52 +0700

    example httpClient 4.3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
-
+import { Component,OnInit } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {UserResponse} from './user-response';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'app';
+  constructor(private http: HttpClient){}
+  ngOnInit():void{
+    this.http.get<UserResponse>('https://api.github.com/users/seeschweiler').subscribe(data => {
+      console.log("User Login: " + data.login);
+      console.log("Bio: " + data.bio);
+      console.log("Company: " + data.company);
+    });
+  }
 }
